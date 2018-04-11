cd57a5feaab33ce208d1c816967e55a1fdefc7f0
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2018-04-11 06:14:40 +0300

    Update WRITER.rb

--- moduls/WRITER.rb
+++ moduls/WRITER.rb
@@ -1,13 +1,14 @@
-  module Writer
+module Writer
   class ::File
     def write_line(str)
-      self.write(str)
-      self.write("\n")
+      write(str)
+      write("\n")
     end
+
     def arr_write(arr)
       arr.each do |word|
-        self.write_line(word)
-     end
+        write_line(word)
+      end
     end
   end
 end
